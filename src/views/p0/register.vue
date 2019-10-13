082a9fd815544dba3c83bd4bcb8b04dc90ee3f11
arfa <arfrix.dev@gmail.com>
2019-10-13 20:36:46 +0330

    hide pass btn in register.vue

--- src/views/p0/register.vue
+++ src/views/p0/register.vue
@@ -28,6 +28,7 @@
                 <h5 :class="placeHolderStyle(input2_isSelected)">رمز عبور</h5>
                 <input  type="text" v-model="passInput" id="passInputId" class="input" autocomplete="off">
                 <div :class="line(input2_isSelected)"></div>
+                <img v-if="input2_isSelected" src="../../assets/icons/view.png" alt="" class="passEye" @click="changePassVisiblity()">
             </div>
         </div>
         <div v-if="showError" class="col-m-10 errorMessage-contaner">
@@ -124,6 +125,14 @@ export default {
         document.getElementById('emailInputId').focus()
         document.getElementById('passInputId').blur()
         document.getElementById('userNameInputId').blur()
+      }
+    },
+    changePassVisiblity () {
+      var x = document.getElementById('passInputId')
+      if (x.type === 'password') {
+        x.type = 'text'
+      } else {
+        x.type = 'password'
       }
     }
 
